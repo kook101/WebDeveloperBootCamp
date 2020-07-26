b83748b256ac84bd6dd89bd7aabdd1041c854de6
kook101 <jaekook.h@gmail.com>
2020-07-26 00:04:18 -0400

    primitive datatypes

---
+++ script.js
@@ -0,0 +1,34 @@
+//JavaScript has 5 Primitive Datatypes
+// Numbers
+4
+9.3
+-10 
+
+// Strings(=text)
+"Hello World"
+"43"
+
+// Booleans
+true
+false
+
+//null and undefined
+null
+undefined
+
+
+//remainder operator
+% 
+
+//single or double quotes ok
+"hello world"
+'hello world'
+
+//concatenation
+"charlie" + "brown"
+
+//Escape Chracters start with "\"
+"Singin \"Do wah diddy, diddy, dum diddy do\" "
+
+//Access individual characters using [] and an index
+"hello"[0] //"h"
