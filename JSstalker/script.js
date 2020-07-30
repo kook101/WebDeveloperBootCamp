f0fd4ee07a07123997dd389dfe2d2383f907d06d
kook101 <jaekook.h@gmail.com>
2020-07-30 00:27:37 -0400

    Add var userFullName

--- JSstalker/script.js
+++ JSstalker/script.js
@@ -2,5 +2,7 @@ var userFirstName = prompt("What is your first name?");
 var userLastName = prompt("What is your last name?");
 var userAge = prompt("What is your age?");
 
-console.log("Your full name is " + userFirstName + " " + userLastName + ".");
+var userFullName = userFirstName + " " + userLastName;
+
+console.log("Your full name is " + userFullName + ".");
 console.log("You are " + userAge +" years old.");
