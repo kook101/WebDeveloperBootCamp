8237cd010f25398366a4d21d6ebb09a7063632e5
kook101 <jaekook.h@gmail.com>
2020-09-22 22:43:58 -0700

    loop

---
+++ ControlFlow/Loop/script.js
@@ -0,0 +1,14 @@
+//Dry: don't repeat yourself
+//We want to keep our code as DRY as possible.
+
+//While loops
+// Repeat code while a condition is true
+// while(some condtition) { run some code }
+// always becarefule infinite loop
+
+var num = 1;
+
+while( num <= 10){
+    console.log(num);
+    num += 2;
+}
